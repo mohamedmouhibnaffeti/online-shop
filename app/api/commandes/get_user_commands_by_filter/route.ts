a430c98a327afa5747544e66625a37e73c88d892
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-17 11:58:03 +0100

    get user commands by filter

--- app/api/commandes/get_user_commands_by_filter/route.ts
+++ app/api/commandes/get_user_commands_by_filter/route.ts
@@ -1,14 +1,15 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import db from "@/lib/ConnectDB";
 
-export async function GET(request: NextRequest) {
+export async function GET(request: NextRequest){
     try{
         const email = request.nextUrl.searchParams.get("email")
-        
+        const filter = request.nextUrl.searchParams.get("filter")
+
         db.run("BEGIN TRANSACTION")
 
         const commandes = await new Promise<any>((resolve, reject) => {
-            db.all(`SELECT * FROM commandes WHERE user = ?`, [email], (err, row) => {
+            db.all(`SELECT * FROM commandes WHERE user = ? and state = ?`, [email, filter], (err, row) => {
                 if(err){
                     reject(err)
                     return
@@ -16,11 +17,10 @@ export async function GET(request: NextRequest) {
                 resolve(row)
             })
         })
-
         db.run("COMMIT")
-
         return NextResponse.json({commandes: commandes}, {status: 200})
     }catch(err){
+        console.log(err)
         return NextResponse.json({message: "Internal Server Error."}, {status: 500})
     }
 }
