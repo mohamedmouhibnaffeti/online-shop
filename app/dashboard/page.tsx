67e79dde208365c4e44fcefa93008b654de33d70
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-20 18:49:31 +0100

    Update

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -13,7 +13,7 @@ export default function DashMain(){
                     alert={false}
                 />
                 <SidebarItem
-                    icon={<Sell size={20} />}
+                    icon={< size={20} />}
                     text={"Articles"}
                     active={false}
                     alert={false}
