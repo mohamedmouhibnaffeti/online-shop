7c3f96f0c541fbadc4f02587b0e6252b66a37e1e
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-20 19:11:36 +0100

    Update

--- app/Components/SideBarItem.tsx
+++ app/Components/SideBarItem.tsx
@@ -1,5 +1,6 @@
 import { Divide } from "lucide-react";
 export default function SidebarItem({icon, text, active, alert}:{icon: any, text: any, active: any, alert: any} ){
+    const {expanded}
     return(
         <li className={`
             text-black relative flex items-center py-2 px-3 my-1
