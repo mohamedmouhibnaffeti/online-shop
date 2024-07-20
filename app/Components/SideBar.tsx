32448a00f7f0512279ff61eef3e63f3a6bf69929
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-20 19:05:50 +0100

    Update

--- app/Components/SideBar.tsx
+++ app/Components/SideBar.tsx
@@ -27,7 +27,7 @@ export default function Sidebar({children}: {children: any}){
                                 <span className="text-xs text-gray-600"> mouhibnaffeti@gmail.com </span>
                             </div>
                         </div>
-                        <LogOut className="text-red-500 cursor-pointer hover:text-red-700 transition delay-75 duration-100" />
+                        {}
                     </div>
                 </div>
 
