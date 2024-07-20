48c8f9da9ca485415687b3b4732de78506b0d3b1
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-20 16:14:23 +0100

    Update

--- app/Components/SideBar.tsx
+++ app/Components/SideBar.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 export default function Sidebar(){
     return(
         <div className="h-screen">
-            <nav className="h-full flex flex-col bg-white border-r shadow-sm">
+            <nav className="h-full flex flex-col bg-white border-r shadow-sm w-fit">
                 <div className="p-4 pb-2 flex justify-between items-center">
                     <Image src={`https://img.logoipsum.com/243.svg`} height={100} width={100} alt="" className="w-32" />
                     <button className="p-1.5 rounded-lg bg-gray-50 hover:bg-gray-100 text-black"> <ChevronFirst /> </button>
