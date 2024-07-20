0b895fa0129cadce3284e620764045c17d4eb9e4
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-20 16:12:29 +0100

    Update

--- app/Components/SideBar.tsx
+++ app/Components/SideBar.tsx
@@ -6,7 +6,7 @@ export default function Sidebar(){
             <nav className="h-full flex flex-col bg-white border-r shadow-sm">
                 <div className="p-4 pb-2 flex justify-between items-center">
                     <Image src={`https://img.logoipsum.com/243.svg`} height={100} width={100} alt="" className="w-32" />
-                    <button className=""> <ChevronFirst /> </button>
+                    <button className="p-1.5 rounded-lg"> <ChevronFirst /> </button>
                 </div>
             </nav>
         </div>
