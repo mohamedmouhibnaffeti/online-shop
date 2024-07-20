b91fac32e7ebedf31ea1e24e5c6a82a8e6ac72b1
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-20 18:48:52 +0100

    Update

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -13,7 +13,7 @@ export default function DashMain(){
                     alert={false}
                 />
                 <SidebarItem
-                    icon={<ScreenShareIcon size={20} />}
+                    icon={<Article size={20} />}
                     text={"Articles"}
                     active={false}
                     alert={false}
