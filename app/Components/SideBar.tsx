98514d80e111894f3be057c8ff1aee885ef11802
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-20 16:28:23 +0100

    Update

--- app/Components/SideBar.tsx
+++ app/Components/SideBar.tsx
@@ -16,7 +16,7 @@ export default function Sidebar({children}: {children: any}){
                 <div className="border-t border-gray-300 flex p-3">
                     <Image src={`https://ui-avatars.com/api/?bold=true`} height={100} width={100} alt="" className="w-10 h-10 rounded-md" />
                     <div className="flex justify-between items-center w-52 ml-3">
-                        <div>
+                        <div >
                             <h4 className="font-semibold"> John Doe </h4>
                         </div>
                     </div>
