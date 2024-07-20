c30796b50752c4be286ac9346fa9af37e7a0877c
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-20 16:05:13 +0100

    Update

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,9 @@
+import Sidebar from "../Components/SideBar";
+
 export default function DashMain(){
     return(
-        <div className="">
-
+        <div>
+            <Sidebar />
         </div>
     )
 }
