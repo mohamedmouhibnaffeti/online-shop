c9a7c7364813dc29e6c7749c5182d5e75daba83c
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-20 18:44:01 +0100

    Update

--- app/Components/SideBarItem.tsx
+++ app/Components/SideBarItem.tsx
@@ -1,6 +1,6 @@
 export default function SidebarItem({icon, text, active, alert}:{icon: any, text: any, active: any, alert: any} ){
     return(
-        <li className="text-back">
+        <li className="text-black">
             {icon}
             <span> {text} </span>
         </li>
