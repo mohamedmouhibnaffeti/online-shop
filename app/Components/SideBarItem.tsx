0171db84b574d6fb3a7ed5b0001d1826f4e77ca4
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-20 18:45:45 +0100

    Update

--- app/Components/SideBarItem.tsx
+++ app/Components/SideBarItem.tsx
@@ -3,7 +3,7 @@ export default function SidebarItem({icon, text, active, alert}:{icon: any, text
         <li className={`
             text-black relative flex items-center py-2 px-3 my-1
             font-medium rounded-md cursor-pointer transition-colors
-            
+            ${active ? "bg-gradient"}
         `}>
             {icon}
             <span> {text} </span>
