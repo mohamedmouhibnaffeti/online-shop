b4e03ce6f8949221968abd0ea60fec4b93c7f4c4
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-20 16:10:35 +0100

    Update

--- app/Components/SideBar.tsx
+++ app/Components/SideBar.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 export default function Sidebar(){
     return(
         <div className="h-screen">
-            <nav className="h-full flex flex-col bg-white border-r shadow-sm">
+            <nav className="h-full flex flex-col bg-white border-r border-2 shadow-sm">
                 <div className="p-4 pb-2 flex justify-between items-center">
                     <Image src={`https://img.logoipsum.com/243.svg`} height={100} width={100} alt="" className="w-32" />
                 </div>
