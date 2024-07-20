64431b3a1baac7bfd1b05540bdb38f9e66612373
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-20 18:44:30 +0100

    Update

--- app/Components/SideBarItem.tsx
+++ app/Components/SideBarItem.tsx
@@ -1,6 +1,6 @@
 export default function SidebarItem({icon, text, active, alert}:{icon: any, text: any, active: any, alert: any} ){
     return(
-        <li className="text-black relative flex items-center py-2">
+        <li className="text-black relative flex items-center py-2 px-3 my-1 font-medium">
             {icon}
             <span> {text} </span>
         </li>
