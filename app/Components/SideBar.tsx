38989bd141cff24c6bdad315d155cd93b98fd769
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-20 16:24:39 +0100

    Update

--- app/Components/SideBar.tsx
+++ app/Components/SideBar.tsx
@@ -14,7 +14,7 @@ export default function Sidebar({children}: {children: any}){
                 </ul>
 
                 <div className="border-t border-gray-300 flex p-3">
-                <Image src={`https://ui-avatars.com/api/?name=John+Doe&bold=truesize`} height={100} width={100} alt="" className="w-32" />
+                <Image src={`https://ui-avatars.com/api/?name=John+Doe&bold=true&size=8`} height={100} width={100} alt="" className="w-32" />
                 </div>
 
             </nav>
