6f6ebd460e9ea75ad10b6e449cf7798ecaccf27e
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-20 16:17:59 +0100

    Update

--- app/Components/SideBar.tsx
+++ app/Components/SideBar.tsx
@@ -1,6 +1,6 @@
 import { ChevronFirst } from "lucide-react"
 import Image from "next/image"
-export default function Sidebar(){
+export default function Sidebar({children}){
     return(
         <div className="h-screen">
             <nav className="h-full flex flex-col bg-white border-r border-gray-300 shadow-sm w-fit">
@@ -8,7 +8,9 @@ export default function Sidebar(){
                     <Image src={`https://img.logoipsum.com/243.svg`} height={100} width={100} alt="" className="w-32" />
                     <button className="p-1.5 rounded-lg bg-gray-50 hover:bg-gray-100 text-black"> <ChevronFirst /> </button>
                 </div>
-                <ul className="flex-1 px-3"></ul>
+                <ul className="flex-1 px-3">
+                    {children}
+                </ul>
             </nav>
         </div>
     )
