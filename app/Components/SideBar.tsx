41b15b548734823bddc61abba0ebf6fe8044b882
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-20 16:04:11 +0100

    Update

---
+++ app/Components/SideBar.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image"
+export default function Sidebar(){
+    return(
+        <div className="h-screen">
+            <nav className="h-full flex flex-col bg-white border-r shadow-sm">
+                <div className="p-4 pb-2 flex justify-between items-center">
+                    <Image src={`https://img.logoipsum.com/243.svg`} alt="" className="w-32" />
+                </div>
+            </nav>
+        </div>
+    )
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function DashMain(){
+    return(
+        <div>
+
+        </div>
+    )
+}
