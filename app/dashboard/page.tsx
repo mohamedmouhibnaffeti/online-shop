2ff57ea0b19efb74ac26a891710697420a59586a
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-20 16:36:36 +0100

    Update

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,7 +3,9 @@ import Sidebar from "../Components/SideBar";
 export default function DashMain(){
     return(
         <div className="bg-white">
-            <Sidebar />
+            <Sidebar>
+                
+            </Sidebar>
         </div>
     )
 }
