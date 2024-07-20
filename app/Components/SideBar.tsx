9be7b619935f57916ccda7584e3a14ebbd70fef7
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-20 16:31:21 +0100

    Update

--- app/Components/SideBar.tsx
+++ app/Components/SideBar.tsx
@@ -14,7 +14,7 @@ export default function Sidebar({children}: {children: any}){
                 </ul>
 
                 <div className="border-t border-gray-300 flex p-3">
-                    <Image src={`https://ui-avatars.com/api/?name=MN&bold=true&background=ADD8E6&color=`} height={100} width={100} alt="" className="w-10 h-10 rounded-md" />
+                    <Image src={`https://ui-avatars.com/api/?name=MN&bold=true&background=ADD8E6&color=4682B4`} height={100} width={100} alt="" className="w-10 h-10 rounded-md" />
                     <div className="flex justify-between items-center w-52 ml-3">
                         <div className="leading-4">
                             <h4 className="font-semibold text-black"> John Doe </h4>
