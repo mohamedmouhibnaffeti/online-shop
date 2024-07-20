ef1e3f28dea2193bc9a653959670820813b1db7e
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-20 16:05:51 +0100

    Update

--- app/Components/SideBar.tsx
+++ app/Components/SideBar.tsx
@@ -4,7 +4,7 @@ export default function Sidebar(){
         <div className="h-screen">
             <nav className="h-full flex flex-col bg-white border-r shadow-sm">
                 <div className="p-4 pb-2 flex justify-between items-center">
-                    <Image src={`https://img.logoipsum.com/243.svg`} alt="" className="w-32" />
+                    <Image src={`https://img.logoipsum.com/243.svg`} height={100} width={100} alt="" className="w-32" />
                 </div>
             </nav>
         </div>
