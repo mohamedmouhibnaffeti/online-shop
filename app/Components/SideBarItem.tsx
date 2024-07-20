fa90962fe259fa698e8f5e346fef5e3dca9c8280
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-20 19:16:59 +0100

    Update

--- app/Components/SideBarItem.tsx
+++ app/Components/SideBarItem.tsx
@@ -11,7 +11,7 @@ export default function SidebarItem({icon, text, active, alert}:{icon: any, text
         `}>
             {icon}
             <span className={`overflow-hidden transition-all ${expanded ? "w-52 ml-3" : "w-0"}`}> {text} </span>
-            {alert && <div className={`absolute right-2 w-2 h-2 rounded bg-indigo-400 ${expanded ? "" : ""}`} />}
+            {alert && <div className={`absolute right-2 w-2 h-2 rounded bg-indigo-400 ${expanded ? "" : "top-2"}`} />}
         </li>
     )
 }
