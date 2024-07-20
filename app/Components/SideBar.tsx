9b0fba841641184232f01126a2d43b68fa8461cf
Mohamed_Mouhib_Naffeti <mouhamednaffeti77@gmail.com>
2024-07-20 16:18:12 +0100

    Update

--- app/Components/SideBar.tsx
+++ app/Components/SideBar.tsx
@@ -1,6 +1,6 @@
 import { ChevronFirst } from "lucide-react"
 import Image from "next/image"
-export default function Sidebar({children}){
+export default function Sidebar({children}: {children: any}){
     return(
         <div className="h-screen">
             <nav className="h-full flex flex-col bg-white border-r border-gray-300 shadow-sm w-fit">
